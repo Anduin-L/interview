b60622146666a17e65d34eb16bd84475244a8a57
LIJIANGBO 李江波 <anduin.li@wework.cn>
2023-12-01 23:01:40 +0800

    algorithm: 快速排序

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+
+#include "src/Algorithm/QuickSort.cpp"
 
 int main() {
-  std::cout << "Hello" << std::endl;
+  std::vector<int> data{1, 6, 7, 3};
+//  quick_sort_r_pivot(data, 0, data.size() - 1);
+  quick_sort(data);
+  for (int num: data) {
+    std::cout << num << std::endl;
+  }
   return 0;
 }
